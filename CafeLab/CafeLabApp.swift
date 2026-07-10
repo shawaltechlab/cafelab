2c968b42c9722b33ef11aa0a4631d46c1456f503
m.shawal <m.shawal@hitachi-ebworx.com>
2026-07-11 07:39:20 +0800

    initialized code base

---
+++ CafeLab/CafeLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeLabApp.swift
+//  CafeLab
+//
+//  Created by Mohamad Shawal Sapuan Bin Mohamad on 11/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct CafeLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CafeLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CafeLab
+//
+//  Created by Mohamad Shawal Sapuan Bin Mohamad on 11/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
